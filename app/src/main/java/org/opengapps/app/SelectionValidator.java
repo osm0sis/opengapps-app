e9c3b452bad155e110d4f8f2978bfcc7beb28695
mfonville <maarten.fonville@gmail.com>
2016-10-24 09:56:54 +0200

    Enable Android 7.1

--- app/src/main/java/org/opengapps/app/SelectionValidator.java
+++ app/src/main/java/org/opengapps/app/SelectionValidator.java
@@ -15,9 +15,6 @@ public class SelectionValidator {
     }
 
     public static boolean isValidArchAnd(String arch, String android) {
-        if (android.equals("7.1")) {
-            return false;
-        }
         return !(arch.contains("64") && android.equals("4.4"));
     }
 
