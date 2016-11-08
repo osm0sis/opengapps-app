11bcd029b0e8cd06e22038bc8333c2e9ee796be5
codekidX <ckrackerx15@gmail.com>
2016-11-08 23:05:54 +0530

    SplashScreen: improvements in design + no exit animation

--- app/src/main/java/org/opengapps/app/SplashActivity.java
+++ app/src/main/java/org/opengapps/app/SplashActivity.java
@@ -2,9 +2,13 @@ package org.opengapps.app;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import org.opengapps.app.intro.AppIntroActivity;
 import org.opengapps.app.prefs.Preferences;
@@ -17,8 +21,15 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(), R.color.splash_status_color));
+        }
+
         SharedPreferences pref = getSharedPreferences(Preferences.prefName, MODE_PRIVATE);
         Handler mHandler = new Handler();
+
+        TextView splashText = (TextView) findViewById(R.id.splash_text);
+        splashText.setTypeface(Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/futura.ttf"));
 
         int SPLASH_DELAY = 2000;
         String SPLASH_KEY = "firstStart";
@@ -53,5 +64,6 @@ public class SplashActivity extends AppCompatActivity {
         Intent i = new Intent(this, activityClass);
         startActivity(i);
         this.finish();
+        this.overridePendingTransition(0,0);
     }
 }
