7febfb19f8da21da69cdf8cf387c8e0ea8fcf078
Christoph Loy <loy.christoph@gmail.com>
2016-11-24 18:16:21 +0100

    night-mode! Yay!

--- app/src/main/java/org/opengapps/app/OGappsApplication.java
+++ app/src/main/java/org/opengapps/app/OGappsApplication.java
@@ -1,17 +1,17 @@
 package org.opengapps.app;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatDelegate;
 
 import com.google.firebase.crash.FirebaseCrash;
 
+import org.opengapps.app.prefs.Preferences;
+
 
 public class OGappsApplication extends Application {
-    static {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-    }
-
     public OGappsApplication() {
+        super();
         Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
 
             @Override
@@ -21,5 +21,13 @@ public class OGappsApplication extends Application {
                 }
             }
         });
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SharedPreferences preferences = getSharedPreferences(Preferences.prefName, MODE_PRIVATE);
+        if (preferences.getBoolean("nightMode", false))
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
 }
