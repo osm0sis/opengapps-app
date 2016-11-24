4c99a46c137faa146305ebbfd4e701c05dde41a4
Christoph Loy <loy.christoph@gmail.com>
2016-11-24 16:28:11 +0100

    merge

--- app/src/main/java/org/opengapps/app/OGappsApplication.java
+++ app/src/main/java/org/opengapps/app/OGappsApplication.java
@@ -1,11 +1,16 @@
 package org.opengapps.app;
 
 import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.google.firebase.crash.FirebaseCrash;
 
 
 public class OGappsApplication extends Application {
+    static {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
     public OGappsApplication() {
         Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
 
