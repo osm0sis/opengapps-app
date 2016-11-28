b4a0fa6a95ced04892cfcd4229e3ad8d3576644a
Ashish <ckrackerx15@gmail.com>
2016-11-28 22:04:15 +0530

    create rate and support cards

---
+++ app/src/main/java/org/opengapps/app/card/RateUsCard.java
@@ -0,0 +1,35 @@
+package org.opengapps.app.card;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import org.opengapps.app.R;
+
+public class RateUsCard extends CardView {
+
+    Button rateButton;
+    Button laterButton;
+
+    public RateUsCard(Context context) {
+        super(context);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.rate_us_card, this, true);
+        initButtons();
+    }
+
+    public void initButtons() {
+        rateButton = (Button) findViewById(R.id.rate_button);
+        laterButton = (Button) findViewById(R.id.later_button);
+    }
+
+    public void setRateListener(View.OnClickListener clickListener) {
+        rateButton.setOnClickListener(clickListener);
+    }
+
+    public void setLaterListener(View.OnClickListener clickListener) {
+        laterButton.setOnClickListener(clickListener);
+    }
+}
---
+++ app/src/main/java/org/opengapps/app/card/SupportCard.java
@@ -0,0 +1,35 @@
+package org.opengapps.app.card;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import org.opengapps.app.R;
+
+public class SupportCard extends CardView {
+
+    Button supportButton;
+    Button laterButton;
+
+    public SupportCard(Context context) {
+        super(context);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.support_card, this, true);
+        initButtons();
+    }
+
+    public void initButtons() {
+        supportButton = (Button) findViewById(R.id.support_button);
+        laterButton = (Button) findViewById(R.id.later_button);
+    }
+
+    public void setSupportButton(View.OnClickListener clickListener) {
+        supportButton.setOnClickListener(clickListener);
+    }
+
+    public void setLaterListener(View.OnClickListener clickListener) {
+        laterButton.setOnClickListener(clickListener);
+    }
+}
